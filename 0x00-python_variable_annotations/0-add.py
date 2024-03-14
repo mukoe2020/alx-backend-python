610bbcdfba85e53aa8a38d2b5cb62d1c8ba7b4c9
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-15 01:16:13 +0200

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+ type-annotated function add that takes a float a and a float b
+ """
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns the sum of two floats
+    """
+    return a + b
