79df83af6e5dc6860e36a9897bac48decf87a0a6
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-15 01:20:47 +0200

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function concat takes a string str1 and a string str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string
+    """
+    return str1 + str2
