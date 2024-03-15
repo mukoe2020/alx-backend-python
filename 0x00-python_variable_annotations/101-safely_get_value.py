9ff17539f13c13cebd34ef5250e9563eba4d6dd0
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-15 02:20:02 +0200

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,3 +6,14 @@ from typing import Any, Mapping, TypeVar, Union
 
 
 T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] =
+                     None) -> Union[Any, T]:
+    """
+    Return first element of a sequence
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
