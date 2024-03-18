70062c86e94731088f4e9de1ebb7c930f7633f40
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-18 16:34:36 +0200

    excute multiple coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
- write an async routine called wait_n that takes in 2 int arguments : n and max_delay 
+an async routine called wait_n that takes in 2 int arguments : n and max_delay
 """
-import
+import random
 import asyncio
 from typing import List
 
@@ -21,4 +21,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     list_of_delays = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(list_of_delays)]
-
